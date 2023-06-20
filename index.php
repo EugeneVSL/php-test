1fc650c3c5d5c4a66ff0a8bcd7c140622617017f
Eugene <eugenevasile@gmail.com>
2023-06-20 17:36:31 +0200

    PDO
    PDO

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+$dsn = "mysql:host=localhost;port=3306;dbname=php-test;charset=utf8mb4";
+$usr = "php-test-usr";
+$pwd = "GMn^7Vme0qY8";
+
+$pdo = new PDO($dsn, $usr, $pwd);
+$statement = $pdo->prepare("select * from posts where id = :id");
+$statement->execute(['id' => 1]);
+$post = $statement->fetch(PDO::FETCH_ASSOC);
+
+echo "<li>" . $post['title'] . "</li>";
+
+
+
+
