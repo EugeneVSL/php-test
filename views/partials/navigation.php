f6dc5fb27152daa7f563f370c6348e678460681a
Eugene <eugenevasile@gmail.com>
2023-07-01 22:04:11 +0200

    Quick update
    Quick update

--- views/partials/navigation.php
+++ views/partials/navigation.php
@@ -12,8 +12,12 @@
                         <a href="/php-test"
                             class="<?= urlIs('/php-test/') ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium' ?>"
                             aria-current="page">Home</a>
+
+                        <?php if ($_SESSION['user'] ?? false) : ?>
                         <a href="/php-test/about"
                             class="<?= urlIs('/php-test/about.php') ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium' ?>">About</a>
+                        <?php endif ?>
+                        
                         <a href="/php-test/notes"
                             class="<?= urlIs('/php-test/notes.php') ? 'bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium' : 'text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium' ?>">Notes</a>
                         <a href="/php-test/contact"
